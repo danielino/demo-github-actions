dc31257ef5c186e5d95eb901aee21cd5b62d39d6
Daniele Marcocci <daniele.marcocci@outlook.com>
2020-10-16 14:24:26 +0200

    add functions

--- demo/test_utils.py
+++ demo/test_utils.py
@@ -3,3 +3,7 @@ from demo import utils
 
 def test_utils_somma():
     assert utils.somma(1, 2) == 3
+
+
+def test_utils_moltiplicazione():
+    assert utils.moltiplicazione(2, 2) == 4
--- demo/utils.py
+++ demo/utils.py
@@ -1,9 +1,30 @@
+import datetime
+from typing import Any
 
-def somma(a, b):
+
+def somma(a, b) -> Any[int, float]:
     """
     function that return sum of args
     :param a:
     :param b:
-    :return:
+    :return: int|float
     """
     return a + b
+
+def moltiplicazione(a, b) -> Any[int, float]:
+    """
+    function that return multiplication of args
+    :param a:
+    :param b:
+    :return:
+    """
+    return a * b
+
+
+def get_date_iso_now() -> str:
+    """
+    return current date time in iso format
+    :return: str
+    """
+    return datetime.datetime.now().isoformat()
+
